b81edbadc43bd0180518fa6d19e3f3eb5d25eebd
Tapish Rawat <iamtapishrawat@gmail.com>
2023-01-19 00:34:09 +0530

    Update 1. Two Sum(Optimized).java

--- 1. Two Sum/1. Two Sum(Optimized).java
+++ 1. Two Sum/1. Two Sum(Optimized).java
@@ -5,17 +5,21 @@
 
 class Solution {
     public int[] twoSum(int[] n, int target) {
-        HashMap<Integer,Integer> mp = new HashMap<>(); 
+       //Phele HUm hashMap ka use krengy aur nums array ke saare elements with unke indexes
+       //(hum indexes isliye store krey h kuki hume return krna h index.
+        HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i = 0; i < n.length; i++){
             mp.put(n[i],i);
         }
         
         int []ans = new int[2];
+       //Ab hum loop chalengy aur check krengy ki given target-nums[i] value humare hashmap me 
+       //present ha ya nhi...
         for(int i=0 ; i< n.length; i++){
             int check = target - n[i];
             if(mp.containsKey(check) && mp.get(check) != i){
-                ans[0] = i;
-                ans[1] = mp.get(check);
+                ans[0] = i;//Agar ha toh ans[0] me i ko store krdengy
+                ans[1] = mp.get(check);//aur mp.get(check) ko ans[1] me
                 return ans;
              }
 
